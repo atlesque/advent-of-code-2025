d7fb3d337de480a031ed87c9f9a551635df5ba27
Alex Van Maele <alexander.vanmaele@vlaanderen.be>
2025-12-08 21:56:17 +0100

    feat: add tests for findClosestPair function using test input

--- days/8/part-1/findClosestPair.spec.ts
+++ days/8/part-1/findClosestPair.spec.ts
@@ -1,4 +1,6 @@
+import { readFileSync } from 'fs';
 import { findClosestPair } from './findClosestPair';
+import { parseCoordinatesList } from './parseCoordinatesList';
 
 describe('findClosestPair', () => {
   it('should find the closest pair of coordinates from a list', () => {
@@ -11,6 +13,16 @@ describe('findClosestPair', () => {
       { x: 4, y: 5, z: 6 },
     ];
     const result = findClosestPair(input);
+    expect(result).toEqual(expectedOutput);
+  });
+  it('should solve the test input correctly', () => {
+    const input = readFileSync('./days/8/part-1/test-input.txt', 'utf-8');
+    const coordinatesList = parseCoordinatesList(input);
+    const expectedOutput = [
+      { x: 162, y: 817, z: 812 },
+      { x: 425, y: 690, z: 689 },
+    ];
+    const result = findClosestPair(coordinatesList);
     expect(result).toEqual(expectedOutput);
   });
 });
